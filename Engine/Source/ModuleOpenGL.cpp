1b05310fce10d4d17013b68aac70b58395c4f4ab
Ariadna Cortes <83670087+acpuig@users.noreply.github.com>
2023-10-23 11:49:59 +0200

    Can't find glew32.lib:, resta ok

--- Engine/Source/ModuleOpenGL.cpp
+++ Engine/Source/ModuleOpenGL.cpp
@@ -4,14 +4,12 @@
 #include "ModuleWindow.h"
 #include "SDL.h"
 
+#include <GL/glew.h>
+
+
 ModuleOpenGL::ModuleOpenGL()
 {
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4); // desired version
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1); // we want a double buffer
-	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24); // we want to have a depth buffer with 24 bits
-	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8); // we want to have a stencil buffer with 8 bit
+
 }
 
 // Destructor
@@ -22,13 +20,44 @@ ModuleOpenGL::~ModuleOpenGL()
 // Called before render is available
 bool ModuleOpenGL::Init()
 {
-	LOG("Creating Renderer context");
+	//Setup attributes
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4); // desired version
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1); // we want a double buffer
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24); // we want to have a depth buffer with 24 bits
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8); // we want to have a stencil buffer with 8 bit
 
+	LOG("Creating Renderer context");
+	SDL_GL_CreateContext(App->GetWindow()->window);
+
+	//SDL_WINDOW_OPENGL();  Remember to create the SDL window with special flag
+
+	//Init the GLEW library
+ 	GLenum err = glewInit();
+	// … check for errors
+	LOG("Using Glew %s", glewGetString(GLEW_VERSION));
+	// Should be 2.0
+
+	//Detect current hardware and driver capabilities
+	LOG("Vendor: %s", glGetString(GL_VENDOR));
+	LOG("Renderer: %s", glGetString(GL_RENDERER));
+	LOG("OpenGL version supported %s", glGetString(GL_VERSION));
+	LOG("GLSL: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+	//Initialize global states
+	glEnable(GL_DEPTH_TEST); // Enable depth test
+	glEnable(GL_CULL_FACE); // Enable cull backward faces
+	glFrontFace(GL_CCW); // Front faces will be counter clockwise*/
 	return true;
 }
 
 update_status ModuleOpenGL::PreUpdate()
 {
+	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 	return UPDATE_CONTINUE;
 }
 
@@ -42,6 +71,7 @@ update_status ModuleOpenGL::Update()
 update_status ModuleOpenGL::PostUpdate()
 {
 	//change backbuffer y front buffer 
+	SDL_GL_SwapWindow(App->GetWindow()->window);
 	return UPDATE_CONTINUE;
 }
 
@@ -51,7 +81,7 @@ bool ModuleOpenGL::CleanUp()
 	LOG("Destroying renderer");
 
 	//Destroy window
-
+	SDL_GL_DeleteContext(App->GetWindow()->window);
 	return true;
 }
 
